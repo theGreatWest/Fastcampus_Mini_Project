68f80c414d1c5cdd098a2e8ce565bee79ddf63a9
youngjuKim <kjuzoojuk@naver.com>
2025-02-02 17:21:55 +0900

    feat: Object 데이터의 자료형 반환 메서드와 Object 데이터를 특정 자료형으로 변환하는 메서드는 추가

--- src/main/java/org/example/util/FindOriginDataFormat.java
+++ src/main/java/org/example/util/FindOriginDataFormat.java
@@ -8,36 +8,43 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FindOriginDataFormat {
-    public static Object start(Cell cell){
+    public static Object findOriginData(Cell cell) {
         switch (cell.getCellType()) {
             case NUMERIC:
                 if (DateUtil.isCellDateFormatted(cell)) {
                     Date dateValue = cell.getDateCellValue();
                     DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                    String formattedDate = dateFormat.format(dateValue);
-                    return formattedDate;
+                    return dateFormat.format(dateValue);
                 } else {
                     double numericValue = cell.getNumericCellValue();
                     if (numericValue == Math.floor(numericValue)) { // 버림
-                        int intValue = (int) numericValue;
-                        return intValue;
+                        return (int) numericValue;
                     } else {
                         return numericValue;
                     }
                 }
             case STRING:
-                String stringValue = cell.getStringCellValue();
-                return stringValue;
+                return cell.getStringCellValue();
             case BOOLEAN:
-                boolean booleanValue = cell.getBooleanCellValue();
-                return booleanValue;
+                return cell.getBooleanCellValue();
             case FORMULA:
-                String formulaValue = cell.getCellFormula();
-                return formulaValue;
+                return cell.getCellFormula();
             case BLANK:
                 return null;
             default:
-                return -1;
+                return null;
         }
     }
+//
+//    public static Class<?> findObjType(Object obj) {
+//        if (obj == null) {
+//            return null;
+//        }
+//        return obj.getClass();  // 객체의 클래스 타입을 반환
+//    }
+//
+//    public static <T> T ObjToFindType(Object value, Class<T> targetType) {
+//        if(value==null) return null;
+//        return targetType.cast(value);
+//    }
 }
